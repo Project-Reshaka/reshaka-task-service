cbbc44b1384b0a2e214b26049d7eb8655769e65b
alexey <bolit006.br0m@gmail.com>
2025-11-11 22:14:50 +0300

    True init commit

---
+++ src/main/java/ru/reshaka/taskengine/TaskengineApplication.java
@@ -0,0 +1,13 @@
+package ru.reshaka.taskengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/reshaka/taskengine/TaskengineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.reshaka.taskengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
